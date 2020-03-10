a492834baab4cd4754f3a1cdaad925b92fd821d1
Christina Chapman <cmchapman75@gmail.com>
2020-03-10 11:16:21 -0500

    Added missing SearchBar Component

--- src/SearchBox/SearchBox.js
+++ src/SearchBox/SearchBox.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 class SearchBox extends Component {
     render() {
         return (
             <div className="SearchBox">
-                <FontAwesomeIcon icon={faSearch} />
+                {/* <FontAwesomeIcon icon={faSearch} /> */}
                 <input placeholder="Kale, Spinach, Avocado" />
             </div>
         );
